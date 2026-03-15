7e2f4f8987f891a7f442b594da7e5b8de3524292
Daniel Kalinov <danielkalinov@protonmail.com>
2026-03-15 16:51:29 +0200

    removed redundant fragment

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,14 @@ import { EditorProvider } from "./context/editor-context";
 
 export default function App() {
   return (
-    <>
-      <EditorProvider>
-        <Container>
-          <Header />
-          <Box sx={{ display: "flex", gap: 4 }}>
-            <Editor />
-            <Form />
-          </Box>
-        </Container>
-      </EditorProvider>
-    </>
+    <EditorProvider>
+      <Container>
+        <Header />
+        <Box sx={{ display: "flex", gap: 4 }}>
+          <Editor />
+          <Form />
+        </Box>
+      </Container>
+    </EditorProvider>
   );
 }
