f1add237e344ccfaf999f4a7318f5886dd6ebce2
Daniel Kalinov <danielkalinov@protonmail.com>
2026-03-15 14:04:54 +0200

    added rhf register

--- src/components/field.tsx
+++ src/components/field.tsx
@@ -1,14 +1,17 @@
 import { TextField } from "@mui/material";
 import type { Field } from "../types/field";
+import { useFormContext } from "react-hook-form";
 
 export default function Field({ field }: { field: Field }) {
   const { type, name, label, disabled } = field;
+  const { register } = useFormContext();
 
   function renderField() {
     switch (type) {
       case "text":
         return (
           <TextField
+            {...register(name)}
             type="text"
             name={name}
             label={label}
@@ -18,6 +21,7 @@ export default function Field({ field }: { field: Field }) {
       case "textarea":
         return (
           <TextField
+            {...register(name)}
             type="text"
             name={name}
             label={label}
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,31 +1,39 @@
 import { Box, Button } from "@mui/material";
 import { useEditor } from "../context/editor-context";
 import Fields from "./fields";
+import { FormProvider, useForm } from "react-hook-form";
 
 export default function Form() {
   const { fields } = useEditor();
+  const methods = useForm({
+    // This ensures that when a field is removed from the form, its value is also removed from the form state.
+    shouldUnregister: true,
+  });
 
   const shouldRenderForm = fields.length > 0;
 
   return (
     shouldRenderForm && (
-      <Box
-        component="form"
-        sx={{
-          mt: 4,
-        }}
-      >
-        <Fields />
-        <Button
-          type="submit"
-          variant="contained"
+      <FormProvider {...methods}>
+        <Box
+          component="form"
+          onSubmit={methods.handleSubmit((data) => console.log(data))}
           sx={{
-            mt: 2,
+            mt: 4,
           }}
         >
-          Submit
-        </Button>
-      </Box>
+          <Fields />
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{
+              mt: 2,
+            }}
+          >
+            Submit
+          </Button>
+        </Box>
+      </FormProvider>
     )
   );
 }
