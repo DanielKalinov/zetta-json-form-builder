29d860cfb4359c2b82c38a51c3e8dffad5eb1988
Daniel Kalinov <danielkalinov@protonmail.com>
2026-03-16 11:40:23 +0200

    added read-only monaco-editor as output

--- src/components/output.tsx
+++ src/components/output.tsx
@@ -1,19 +1,24 @@
 import Card from "./card";
 import { useEditor } from "../context/editor-context";
+import { Editor as MonacoEditor } from "@monaco-editor/react";
 
 export default function Output() {
   const { output } = useEditor();
 
   return (
-    <Card
-      label="Output (JSON)"
-      sx={{
-        overflowX: "auto",
-      }}
-    >
-      <pre>
-        <code>{JSON.stringify(output)}</code>
-      </pre>
+    <Card label="Output (JSON)">
+      <MonacoEditor
+        height="10vh"
+        value={JSON.stringify(output)}
+        defaultLanguage="json"
+        options={{
+          fontSize: 16,
+          minimap: {
+            enabled: false,
+          },
+          readOnly: true,
+        }}
+      />
     </Card>
   );
 }
